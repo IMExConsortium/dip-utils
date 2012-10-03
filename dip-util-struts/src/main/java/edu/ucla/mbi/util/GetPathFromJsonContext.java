eb64a034e6f008e4d7012b7109ccbd8971b5e3af
wyu <wyu@a7131d2a-6501-4593-b7ed-533412188f5c>
2012-10-03 00:39:10 +0000

    add a new class GetPathFromJsonContext.java

---
+++ dip-util-struts/src/main/java/edu/ucla/mbi/util/GetPathFromJsonContext.java
@@ -0,0 +1,45 @@
+package edu.ucla.mbi.util;
+
+/*==============================================================================
+ * $HeadURL$ 
+ * $Id$
+ * Version: $Rev$
+ *==============================================================================
+ *
+ * GetPathFromJsonContext 
+ *
+ *==============================================================================
+ */
+
+import java.io.*;
+import javax.servlet.ServletContext;
+
+public class GetPathFromJsonContext {
+    
+    public static String getPath ( JsonContext jsonContext,
+                                   ServletContext servletContext 
+                                   ) throws Exception 
+    {
+
+        String jsonPath =
+                (String) jsonContext.getConfig().get( "json-config" );
+
+        if ( jsonPath != null && jsonPath.length() > 0 ) {
+
+            String cpath = jsonPath.replaceAll("^\\s+","" );
+            cpath = jsonPath.replaceAll("\\s+$","" );
+
+            try {
+                InputStream is = servletContext.getResourceAsStream( cpath );
+                jsonContext.readJsonConfigDef( is );
+            } catch ( Exception e ){
+                throw e;
+            }
+
+            return servletContext.getRealPath( cpath );
+        } else {
+            throw new Exception( "json configuration" ); 
+        }
+    }
+}
+
